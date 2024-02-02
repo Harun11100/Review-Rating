e7e36ec822179747c73536de8a0746fc78b335d9
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-02-02 11:43:09 +0600

    Review raitng is 90 % done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from 'react';
+
+
+import StarRating from './StarRating';
+// function Test(){
+//   const[gameRating,setGamerating]=useState(0);
+//    return<div>
+//     <StarRating  onUserSetRating={setGamerating}/>
+//     <p>This game is rating {gameRating}</p>
+//    </div>
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating/>
+    <StarRating maxrating={5} defaultRating={3}  massage={["terrible","bad","okay","good","amazing"] }/>
+    <StarRating size={9}/>
+    {/* <Test/> */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
